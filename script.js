8a9f5fed4bd39568e8b358a3e300a2fffac0bb50
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-06-13 23:22:06 +0300

    fetch

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 
-const search = document.querySelector('js-search')
+const search = document.querySelector('.js-search')
+const list = document.querySelector('.js-list')
+
 search.addEventListener('submit', onSearch)
 
 function onSearch(evt) {
     evt.preventDefault()
-    console.log(evt.currentTarget.elements);
+
+    const {query, days} = evt.currentTarget.elements
+    getWeather(query.value, days.value)
+.then(data => (list.innerHTML = createMarkup(data.forecast.forecastday)))
+.catch(err => console.log(err))
 }
+
 
 function getWeather(city, days) {
+    //http://api.weatherapi.com/v1/forecast.json?key=ce2cb9b2a3da414bb5b172546231704&q=Paris&days=5
+    const BASE_URL = 'http://api.weatherapi.com/v1';
+    const API_KEY = 'ce2cb9b2a3da414bb5b172546231704';
 
-    // http://api.weatherapi.com/v1/forecast.json?key=ce2cb9b2a3da414bb5b172546231704&q=Paris
-    const BASE_URL = "http://api.weatherapi.com/v1"
-    const API_KEY = "ce2cb9b2a3da414bb5b172546231704"
-
-    return fetch(
-        `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
+return fetch(
+        `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`
     ).then((resp) => {
-        if (!resp.ok) {
-            throw new Error(resp.statusText)
-        }
-        return resp.json()
-    })
+if (!resp.ok) {
+    throw new Error(resp.statusText);
 }
-getWeather("Paris", 5)
-    .then((data) => console.log(data))
-    .then((err) => console.log(err))
+
+return resp.json();
+    });
+}
+
+function createMarkup(arr) {
+    return arr.map(({date, day:{avgtemp_c, condition: {icon, text}}}) => `<li>
+        <img src="${icon}" alt="${text}">
+<p>${text}</p>
+<h2>${date}</h2>
+<h3>${avgtemp_c}</h3>
+</li>`).join('')
+}
